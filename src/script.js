b4d148a204bc1f2643d392f1b8b667a66ba71e78
Juan <juanfrvera@gmail.com>
2019-04-27 23:07:04 -0300

    ajustando nombres de variables

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-var atributos =
+var inputsPrincipal =
 [
     //--Principales
     'nombrePersonaje','clase','raza','alineacion','fondo','experiencia','nombreJugador',
@@ -11,18 +11,20 @@ var atributos =
     'claseDeArmadura','iniciativa','velocidad','vidaMaxima','vidActual','hitDice',
     //--Personalizacion
     'edad','altura','peso','ojos','piel','cabello',
-    //--Historia
+]
+var inputsHistoria = 
+[
     'Historia'
 ]
 
 //Cuando todos los elementos de la página fueron cargados
 window.onload = function(){
-    atributos.forEach(function(nombre){Cargar(nombre)});
+    inputsPrincipal.forEach(function(nombre){Cargar(nombre)});
 }
 
 //Al actualizar o cerrar la página
 window.onbeforeunload = function(){
-    atributos.forEach(function(nombre){Guardar(nombre)});
+    inputsPrincipal.forEach(function(nombre){Guardar(nombre)});
     return null;
 }
 //Prueba commit
