9593839263830e975fc22f78d7df7ec80ccba87e
Juan <juanfrvera@gmail.com>
2019-04-29 01:22:49 -0300

    Selección de habilidades según clase

--- src/principalScript.js
+++ src/principalScript.js
@@ -16,18 +16,77 @@ var inputs =
 ]
 var selects =
 [
-    'clase', 'raza'
+    'raza','clase'
 ]
+var listaHabilidades = "";
 
 //Cuando todos los elementos de la página fueron cargados
 window.onload = function(){
     inputs.forEach(function(nombre){Cargar(nombre)});
     selects.forEach(function(nombre){Cargar(nombre)});
+    listaHabilidades = localStorage.getItem("listaHabilidades");
+    if(listaHabilidades.length > 0){
+        listaHabilidades = listaHabilidades.split(",");
+        EscribirHabilidades();
+    }
+
+    //eval(document.getElementById('clase').value + 'Cantidad') );
 }
 
 //Al actualizar o cerrar la página
 window.onbeforeunload = function(){
     inputs.forEach(function(nombre){Guardar(nombre)});
     selects.forEach(function(nombre){Guardar(nombre)});
+
+    localStorage.setItem("listaHabilidades",listaHabilidades);
+
     return null;
+}
+
+var cantidadHabilidades;
+var divHabilidades;
+function ElegirHabilidades(){
+    var clase = document.getElementById("clase").value;
+    cantidadHabilidades = eval(clase+'Cantidad');
+    divHabilidades = document.getElementById("Habilidades");
+    divHabilidades.innerHTML = '<h4>Habilidades<h4/>';
+    divHabilidades.innerHTML += '<p>Elige '+cantidadHabilidades+' habilidades:</p>';
+    divHabilidades.innerHTML += '<ul id="listaHabilidades"></ul>';
+    var ul = document.getElementById("listaHabilidades");
+    var habilidadesElegibles = eval(clase+'Habilidades');
+    habilidadesElegibles.forEach(function(habilidad){
+        ul.innerHTML+='<li><input name="habilidad" type="checkbox" value="'+habilidad+'">'+habilidad+'</li>'
+    });
+
+    divHabilidades.innerHTML += '<button onclick="AceptarHabilidades()">Aceptar</button>'
+}
+
+function AceptarHabilidades(){
+    var checkboxes = document.getElementsByName("habilidad");
+    var habilidadesElegidas = 0;
+    listaHabilidades = [];
+    checkboxes.forEach(function(checkbox){
+        if(checkbox.checked){
+            habilidadesElegidas += 1;
+            listaHabilidades.push(checkbox.value);
+        }
+    })
+    if(habilidadesElegidas == cantidadHabilidades){
+       EscribirHabilidades();
+    }
+    else
+        alert("Por favor elige "+cantidadHabilidades+" habilidades.");
+}
+function EscribirHabilidades(){
+    divHabilidades = document.getElementById("Habilidades");
+
+    divHabilidades.innerHTML = '<h4>Habilidades</h4>';
+    divHabilidades.innerHTML += '<ul id="listaHabilidades"></ul>';
+    var ul = document.getElementById("listaHabilidades");
+    
+    listaHabilidades.forEach(function(habilidad){
+        ul.innerHTML += '<li>'+habilidad+'</li>';
+    })
+
+    divHabilidades.innerHTML += '<button onclick="ElegirHabilidades()">Cambiar</button>';
 }
--- src/skills.js
+++ src/skills.js
@@ -1,37 +1,69 @@
 //Cada clase puede tener ciertas Habilidades
-
+var habilidades = [
+    'Acrobacias',
+    'Manejo de animales',
+    'Arcana',
+    'Atletismo',
+    'Engaño',
+    'Historia',
+    'Visión',
+    'Intimidación',
+    'Investigación',
+    'Medicina',
+    'Naturaleza',
+    'Percepción',
+    'Actuación',
+    'Persuación',
+    'Religión',
+    'Juego de manos',
+    'Sigilo',
+    'Supervivencia'
+]
 //---barbaro
 //Skills: Choose two from Animal Handling, Athletics,Intimidation, Nature, Perception, and Survival
-
+var barbaroCantidad = 2
+var barbaroHabilidades = ['Manejo de animales', 'Atletismo','Naturaleza','Percepción','Supervivencia']
 //---bardo
 //Skills: Choose any three
-
+var bardoCantidad = 3
+var bardoHabilidades = []
 //---cleric
 //Skills: Choose two from History, lnsight, Medicine,Persuasion, and Religion
-
+var clerigoCantidad = 2
+var clerigoHabilidades = ['Historia', 'Visión', 'Medicina','Persuación','Religión']
 //---druida
 //Skills: Choose two from Arcana, Animal Handling, Insight, Medicine, Nature, Perception,Religion, and Survival
-
+var druidaCantidad = 2
+var druidaHabilidades = ['Arcana','Manejo de animales','Visión','Medicina','Naturaleza','Percepción','Religión','Supervivencia']
 //---figther
 //Skills: Choose two skills from Acrobatics, Animal Handling, Athletics, History, Insight, intimidation,Perception, and Survival
-
+var guerreroCantidad = 2
+var guerreroHabilidades = ['Acrobacias','Manejo de animales','Atletismo','Historia','Visión','Intimidación','Percepción','Supervivencia']
 //---Monje
 //Skills: Choose two from Acrobatics, Athletics, History, lnsight, Religion, and Stealth
-
+var monjeCantidad = 2
+var monjeHabilidades = ['Acrobacias','Atletismo','Historia','Visión','Religión','Sigilo']
 //---Paladin
 //Skills: Choose two from Athletics, Insight, Intimidation, Medicine, Persuasion, and Religion
-
+var paladinCantidad = 2
+var paladinHabilidades = ['Atletismo','Visión','Intimidación','Medicina','Persuación','Religión']
 //---Ranger
 //Skills: Choose three from Animal Handling, Athletics, Insight, [nvestigation, Nature, Perception, Stealth, and Survival
-
+var exploradorCantidad = 3
+var exploradorHabilidades = ['Manejo de animales','Atletismo','Visión','Investigación','Naturaleza','Percepción','Sigilo','Supervivencia']
 //---Picaro
 //Skills: Choose four from Acrobatics, Athletics,Deception, Insight, Intimidation, Investigation, Perception, Pcrformance, Persuasion. Sleight of Hanel, anel Stealth
-
+var picaroCantidad = 4
+var picaroHabilidades = ['Acrobacias','Atletismo','Engaño','Visión','Intimidación','Investigación','Percepción','Actuación','Persuación']
 //---hechicero
 //Skills: Choose two from Arcana, Deception, Insight, Intimidation, Persuasion, and Religion
-
+var hechiceroCantidad = 2
+var hechiceroHabilidades = ['Arcana','Engaño','Visión','Intimidación','Persuación','Religión']
 //---Brujo
 //Skills: Choose lwo skills from Arcana, Deceplion, Hislory, !nlimidalion, Invesligalion, Nalure, and Religion
-
+var brujoCantidad = 2
+var brujoHabilidades = ['Arcana','Engaño','Historia','Intimidación','Investigación','Naturaleza','Religión']
 //---Mago
 //Skills: Choose two from Areana, History, Insight, Investigation, Medicine, and Religion
+var magoCantidad = 2
+var magoHabilidades = ['Arcana','Historia','Visión','Investigación','Medicina','Religión']
