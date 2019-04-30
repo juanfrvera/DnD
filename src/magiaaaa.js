33fdb7e0d0eda89b2d095b4f9ef1e64d250e769e
Lau <alas123789@gmail.com>
2019-04-30 04:43:19 -0300

    spells hechos listas por clase

--- src/magiaaaa.js
+++ src/magiaaaa.js
@@ -4,50 +4,48 @@
 
 // Magia para bardos
 //--Cantrips
-//blade ward, Dancing lights, friends, mage hand, mending, message, minor illusion, presdigitation, true strike, vicious mockery
+var spellsBardo = [ 'blade ward', 'dancing lights', 'friends', 'mage hand', 'mending', 'message', 'minor illusion', 'presdigitation', 'true strike', 'vicious mockery',
 
 //lvl1
-//animal friendship, bane, charm person, comprehend lenguages, cure wounds, detect magic, disguise self, dissonant whispers, faeri fire, feather fall, healing word, heroism, identify, illusory script, longstrider, silent image, sleep, speack with animals, tasha's hideos laughter, thunderwave, unseen servant
+'animal friendship', 'bane', 'charm person', 'comprehend lenguages', 'cure wounds', 'detect magic', 'disguise self', 'dissonant whispers', 'faeri fire', 'feather fall', 'healing word', 'heroism', 'identify', 'illusory script', 'longstrider', 'silent image', 'sleep', 'speack with animals', 'tasha hideos laughter', 'thunderwave', 'unseen servant']
 
 //Magia para clerigo
 //--Cantrips
-//guidance, light, mending, resistance, secred flame, spare the dying, thaumaturgy
+var spellsClerigo = [ 'guidance', 'light', 'mending', 'resistance', 'secred flame', 'spare the dying', 'thaumaturgy',
 
 //lvl1
-//bane, bless, command, create or destroy water, cure wounds,detect evil and good, detect magic, detect poison and disease, guiding bolt, healing word, inflict wounds, protection form, evil and good, purify food and drink, sanctuary, shield of faith
+'bane', 'bless', 'command', 'create or destroy water', 'cure wounds','detect evil and good', 'detect magic', 'detect poison and disease', 'guiding bolt', 'healing word', 'inflict wounds', 'protection form', 'evil and good', 'purify food and drink', 'sanctuary', 'shield of faith']
 
 //Magia para druidas
 //Cantrips
-//druidcraft, guidance, mending, poison spray, produce flame, resistance, shillelagh, thorn whip
+var spellsDruida =  ['druidcraft', 'guidance', 'mending', 'poison spray', 'produce flame', 'resistance', 'shillelagh', 'thorn whip',
 
 //lvl1
-//animal friendship, charm person, create or destroy water, cure wounds, detect magic, detect poison and disease, entangle, feari fire, fog cloud, goodberry, healing word, jump, longstrider, purify food and drink, speack with animals, thunderwave
+'animal friendship', 'charm person', 'create or destroy water', 'cure wounds', 'detect magic', 'detect poison and disease', 'entangle', 'feari fire', 'fog cloud', 'goodberry', 'healing word', 'jump', 'longstrider', 'purify food and drink', 'speack with animals', 'thunderwave']
 
 //Magia para paladines
 //no tienen Cantrips
 //lvl1
-//bless,command, compelled duel, cure wounds, detect evil and good, detect magic, detect poison and disease, divine favor, heroism, protection from evil and good, purify food and drink, searing smite, shield of faith, thunderous smite, wrathful smite
+var spellsPaladines = ['bless','command', 'compelled duel', 'cure wounds', 'detect evil and good', 'detect magic', 'detect poison and disease', 'divine favor', 'heroism', 'protection from evil and good', 'purify food and drink', 'searing smite', 'shield of faith', 'thunderous smite', 'wrathful smite']
 
 //Magia para explorador
 //lvl1
-//alarm, animal friendship, cure wounds, detect magic, detect poison and disease, ensnaring strike, fog clud, goodberry, hail of thorns, hunter's mark, jump, longstrider, speak whit animal
+var spellsExplorador = ['alarm', 'animal friendship', 'cure wounds', 'detect magic', 'detect poison and disease', 'ensnaring strike', 'fog clud', 'goodberry', 'hail of thorns', 'hunter mark', 'jump', 'longstrider', 'speak whit animal']
 
 //magia para hechicero
 //Cantrips
-//acid splash, blade ward, chill touch, dancing lights, fire bolt, friends, light, mage hand, mending, messasge, minor illusion, poison spray, prestigitation, ray of frost, shocking grasp, true strike
+var spellsHechicero = ['acid splash', 'blade ward', 'chill touch', 'dancing lights', 'fire bolt', 'friends', 'light', 'mage hand', 'mending', 'messasge', 'minor illusion', 'poison spray', 'prestigitation', 'ray of frost', 'shocking grasp', 'true strike',
 //lvl1
-//birning hand, charm person, chromatic orb, color spray, comprehend lenguages, detect magic, disguise self, expeditious retreat, false life, feather fall, fog cloud, jump, mage armour, magic missile, ray of sickness, shield, silent image, sleep, thunderwave, witch bolt
+'birning hand', 'charm person', 'chromatic orb', 'color spray', 'comprehend lenguages', 'detect magic', 'disguise self', 'expeditious retreat', 'false life', 'feather fall', 'fog cloud', 'jump', 'mage armour', 'magic missile', 'ray of sickness', 'shield', 'silent image', 'sleep', 'thunderwave', 'witch bolt']
 
 //Magia para brujos
 //Cantrips
-//blade ward, chill touch, eldritch blast, fiends, mage hand, minor illusion, poison spray, presdigitation, true strike
+var spellsBrujo = ['blade ward', 'chill touch', 'eldritch blast', 'fiends', 'mage hand', 'minor illusion', 'poison spray', 'presdigitation', 'true strike',
 //lvl1
-//armor of agathys, arms of hadar, charm person, comprehend lenguages, expeditious retreat, ehellish rebuke, hex, illusory script, protection from evil and good, unseen servant, witch bolt
+'armor of agathys', 'arms of hadar', 'charm person', 'comprehend lenguages', 'expeditious retreat', 'ehellish rebuke', 'hex', 'illusory script', 'protection from evil and good', 'unseen servant', 'witch bolt']
 
 //magia para magos
 //Cantrips
-//acid splash, blade ward, chill tocuh, dancing lights, fire bolt, friends, light, mage hand, mending, message, minor illusion, poison spray, prestidigitation, ray of frost, shocking grasp, true strike
+var spellsMago = ['acid splash', 'blade ward', 'chill tocuh', 'dancing lights', 'fire bolt', 'friends', 'light', 'mage hand', 'mending', 'message', 'minor illusion', 'poison spray', 'prestidigitation', 'ray of frost', 'shocking grasp', 'true strike',
 //lvl1
-//alarm, burning hands, charm person, chromatic orb, color spray, comprehend lenguages, detect magic, disguise self, expeditious retreat, false life, feather fall, find familiar, fog cloud, grease, identify, illusory script, jump, longstrider, mage armor, magic missile, protection form evil and good, ray of sickness, shield, silent image, sleep, tasha's hideous laughter, tenser's floating disk, thunderwave, unseen servant, witch bolt
-
- 
+'alarm', 'burning hands', 'charm person', 'chromatic orb', 'color spray', 'comprehend lenguages', 'detect magic', 'disguise self', 'expeditious retreat', 'false life', 'feather fall', 'find familiar', 'fog cloud', 'grease', 'identify', 'illusory script', 'jump', 'longstrider', 'mage armor', 'magic missile', 'protection form evil and good', 'ray of sickness', 'shield', 'silent image', 'sleep', 'tasha hideous laughter', 'tenser floating disk', 'thunderwave', 'unseen servant', 'witch bolt']
