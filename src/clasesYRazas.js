7074ea26104d06c0d6928b1b4ec77cc53b58c77f
Lau <alas123789@gmail.com>
2019-05-02 15:03:11 -0300

    merge

---
+++ src/clasesYRazas.js
@@ -0,0 +1,38 @@
+var enanoSubRazas = ["De las colinas", "De las montañas"];
+//Medianos
+var medianoSubRazas = ["Pies ligeros","Fornido"];
+var medianoModificadores =
+{
+    destreza : 2
+}
+var medianoPiesLigerosModificadores = 
+{
+    carisma : 1
+}
+var medianoFornidoModificadores =
+{
+    constitucion : 1
+}
+//Humanos
+var humanoSubRazas = ["Calishita","Chondathan","Damaran","Illuskan","Mulan","Rashemi","Shou","Tethyriano","Turami"];
+var humanoModificadores = {
+    fuerza : 1,
+    destreza : 1,
+    constitucion : 1,
+    inteligencia : 1,
+    sabiduria : 1,
+    carisma : 1
+}
+//Dracónido
+var draconidoModificadores = {
+    fuerza : 2,
+    carisma : 1,
+    velocidad : 30
+}
+draconidoSubRazas = ["Azul","Blanco","Bronce","Cobre","Latón","Negro","Oro","Plata","Rojo","Verde"]
+//Gnomos
+var gnomoModificadores = {
+    inteligencia : 2,
+    velocidad : 25
+}
+var gnomoSubRazas = ["Gnomo de los bosques","Gnomo de las rocas"];
